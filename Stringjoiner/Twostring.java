4a5614bff2983c796f5ddc684534bc7d80694b93
Vennelapalla <pallavennela88@gmail.com>
2026-07-13 10:02:09 +0530

    Add files via upload

---
+++ Stringjoiner/Twostring.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class Twostring {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        sc.nextLine();
+
+        StringJoiner s1 = new StringJoiner("-");
+
+        for (int i = 0; i < n1; i++) {
+            s1.add(sc.nextLine());
+        }
+
+        int n2 = sc.nextInt();
+        sc.nextLine();
+
+        StringJoiner s2 = new StringJoiner("-");
+
+        for (int i = 0; i < n2; i++) {
+            s2.add(sc.nextLine());
+        }
+
+        // s1 merged to s2
+        StringJoiner merge1 = new StringJoiner("-");
+        merge1.merge(s2);
+        merge1.merge(s1);
+
+        // s2 merged to s1
+        StringJoiner merge2 = new StringJoiner("-");
+        merge2.merge(s1);
+        merge2.merge(s2);
+
+        System.out.println("s1 merged to s2:");
+        System.out.println(merge1);
+
+        System.out.println("s2 merged to s1:");
+        System.out.println(merge2);
+    }
+}

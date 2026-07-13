36e5513c45b042342f9752797d4369aefd738640
Vennelapalla <pallavennela88@gmail.com>
2026-07-13 21:35:33 +0530

    Add files via upload

---
+++ Inheritance/Inheritance.java
@@ -0,0 +1,21 @@
+package Inheritance;
+
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
+
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}

b7f525505659af0077d8a96dbb6438354d7e4501
Vennelapalla <pallavennela88@gmail.com>
2026-07-13 21:56:41 +0530

    Add files via upload

---
+++ Packages/Compartment.java
@@ -0,0 +1,20 @@
+class Foundation {
+
+    private int var1 = 10;
+    int var2 = 20;
+    protected int var3 = 30;
+    public int var4 = 40;
+}
+
+public class Compartment {
+
+    public static void main(String[] args) {
+
+        Foundation obj = new Foundation();
+
+        // System.out.println(obj.var1); // Private - Not Accessible
+        System.out.println(obj.var2);    // Default - Accessible (same package)
+        System.out.println(obj.var3);    // Protected - Accessible (same package)
+        System.out.println(obj.var4);    // Public - Accessible
+    }
+}

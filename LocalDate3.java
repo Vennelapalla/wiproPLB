313ae29eba04b204494bcd04c17150e2975c96ec
Vennelapalla <pallavennela88@gmail.com>
2026-07-16 11:45:08 +0530

    Delete LocalDate3.java

--- LocalDate3.java
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDate;
-public class LocaDate1 {
-    public static void main(String[] args) {
-        LocalDate today = LocalDate.now();
-        LocalDate afterTenDays = today.plusDays(10);
-        System.out.println("Today: " + today);
-        System.out.println("Date after 10 days: " + afterTenDays);
-    }
-}
